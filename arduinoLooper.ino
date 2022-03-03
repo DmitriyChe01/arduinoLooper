e267640166dec59c983fd1f9761fe189cc1670c8
Dmitriy <d.cherenkov01@gmail.com>
2022-03-03 14:52:09 +0300

    ++

--- arduinoLooper.ino
+++ arduinoLooper.ino
@@ -1,54 +1,55 @@
-// #include <EEPROM.h>
-
+#include <OneButton.h>
+#include <EEPROM.h>
 
 // TODO загрузка из EEPROM
-bool banks[29][3][5]; // Массив банков [банки] [пресеты] [выходы]
-int NO_bank = 0; // Номер банка
-int NO_preset = 0; // Номер пресета
-int NO_input = -1; // Номер входа 
+
+bool banks[30][4][6]; // Массив банков [банки] [пресеты] [выходы]
+uint8_t NO_bank; // Номер банка
+uint8_t NO_preset; // Номер пресета
+uint8_t NO_input = -1; // Номер входа 
+
 
 // Кнопки
-int menu_btn = A2;
-int save_btn = A3;
-int prev_btn = 3;
-int next_btn = 5;
-int a_btn = 6;
-int b_btn = 7;
-int c_btn = A0;
-int d_btn = A1;
+OneButton menu_btn(A2, false, false);
+OneButton save_btn(A3, false, false);
+OneButton prev_btn(3, false, false);
+OneButton next_btn(5, false, false);
+OneButton a_btn(6, false, false);
+OneButton b_btn(7, false, false);
+OneButton c_btn(A0, false, false);
+OneButton d_btn(A1, false, false);
+
+
 
 //Выхода
-int output1 = 2;
-int output2 = 4; 
-int output3 = 8;
-int output4 = 9;
-int output5 = 10;
-int output6 = 12;
-
+const uint8_t led_outputs[6] PROGMEM = {2, 4, 8, 9, 10, 12};
 
 bool menu_mode = false; // Флаг режима меню
 
 void serial_banks_print(){
-	// Выводит банки в сериал порт 
-	Serial.println(' ');
-	for (int i = 0; i < 29; ++i){
-		for (int j = 0; j < 3; ++j){
-			Serial.print('(');
 
-			for (int k = 0; k < 5; ++k){
-				Serial.print(banks[i][j][k]);
-			}
-
-			Serial.print(')');
-		}
-	}
-	Serial.print("\nNO_bank " ); Serial.print(NO_bank);
-	Serial.print("\nNO_preset " ); Serial.print(NO_preset);
-	Serial.print("\nNO_input "); Serial.print(NO_input);
-	Serial.print("\nmenu_mode "); Serial.print(menu_mode);
 }
 
-// utils
+////////
+// UTILS
+////////
+
+void clean_eeprom(){
+	bool banks[30][4][6]; // Массив банков [банки] [пресеты] [выходы]
+	uint8_t NO_bank = 0; // Номер банка
+	uint8_t NO_preset = 0; // Номер пресета
+	for (uint8_t i = 0; i < 30; ++i){
+		for (uint8_t j = 0; j < 4; ++j){
+			for (uint8_t k = 0; k < 6; ++k){
+				banks[i][j][k] = false;
+			}
+			
+		}
+	}
+	EEPROM.put(30, banks);
+	EEPROM.put(1, NO_bank);
+}
+
 void off_menu_mode(){
 	if (menu_mode == true) menu_mode = false;
 	NO_input = -1;
@@ -59,6 +60,7 @@ void on_menu_mode(){
 	NO_input = 0; // Выставляет первый вход
 }
 
+
 void change_input_value(){ // изменить значение входа
 	if (banks[NO_bank][NO_preset][NO_input] == false){
 		banks[NO_bank][NO_preset][NO_input] = true;
@@ -68,22 +70,33 @@ void change_input_value(){ // изменить значение входа
 }
 
 void save_banks(){
-	// TODO сохранить массив в EEPROM
+	EEPROM.put(30, banks);
+}
+
+void save_state(){
+	EEPROM.put(1, NO_bank);
 }
 
 void reset_preset(){ // Сбрасываем пресет
 	NO_preset = 0;
 }
-//handlers
+
+///////////
+// HANDLERS
+///////////
+
 void press_menu_btn(){ // Нажатие на "меню"
-	if (menu_mode == true)
+	if (menu_mode == true){
 		change_input_value(); // Меняет значение
-	else
+	}else{
 		on_menu_mode(); // Включает меню
+	}
 }
 
 void press_save_btn(){ // Нажатие на "сохранить"
 	off_menu_mode();
+	save_banks();
+	Serial.print("\nSAVE!");
 	//TODO Запись массива в EEPROM
 }
 
@@ -119,62 +132,112 @@ void press_next_btn(){ // Нажитие на "next"
 	
 }
 
+void press_a_btn(){
+	NO_preset = 0;
+}
+void press_b_btn(){
+	NO_preset = 1;
+}
+void press_c_btn(){
+	NO_preset = 2;
+}
+void press_d_btn(){
+	NO_preset = 3;
+}
+
+void refrash_led(){	// Обновление светодиодов
+	for (uint8_t i = 0; i < 6; i++){
+		if (banks[NO_bank][NO_preset][i] == true){
+			digitalWrite(led_outputs[i], HIGH);
+		} else {
+			digitalWrite(led_outputs[i], LOW);
+		}
+	}
+}
+
+
 void setup()
 {	
 	Serial.begin(9600);
-	pinMode(menu_btn, INPUT);
-	pinMode(save_btn, INPUT);
-	pinMode(prev_btn, INPUT);
-	pinMode(next_btn, INPUT);
-	pinMode(a_btn, INPUT);
-	pinMode(b_btn, INPUT);
-	pinMode(c_btn, INPUT);
-	pinMode(d_btn, INPUT);
-	pinMode(output1, OUTPUT);
-	pinMode(output2, OUTPUT);
-	pinMode(output3, OUTPUT);
-	pinMode(output4, OUTPUT);
-	pinMode(output5, OUTPUT);
-	pinMode(output6, OUTPUT);
+	
+	// clean_eeprom();
+	serial_banks_print();
+	refrash_led();
+
+	menu_btn.attachClick([](){
+		press_menu_btn();
+		refrash_led();
+		serial_banks_print();
+
+	});
+
+	save_btn.attachClick([](){
+		press_save_btn();
+		refrash_led();
+	});
+
+	prev_btn.attachClick([](){
+		press_prev_btn();
+		refrash_led();
+		serial_banks_print();
+		save_state();
+	});
+
+	next_btn.attachClick([](){
+		press_next_btn();
+		refrash_led();
+		serial_banks_print();
+		save_state();
+	});
+
+	a_btn.attachClick([](){
+		press_a_btn();
+		refrash_led();
+		serial_banks_print();
+		save_state();
+	});
+
+	b_btn.attachClick([](){
+		press_b_btn();
+		refrash_led();
+		serial_banks_print();
+		save_state();	
+	});
+
+	c_btn.attachClick([](){
+		press_c_btn();
+		refrash_led();
+		serial_banks_print();
+		save_state();
+	});
+
+	d_btn.attachClick([](){
+		press_d_btn();
+		refrash_led();
+		serial_banks_print();
+		save_state();
+	});
+
+	for (uint8_t i=0; i < 6; i++)
+		pinMode(led_outputs[i], OUTPUT);
+
+
+
+    EEPROM.get(30, banks);
+	EEPROM.get(1, NO_bank);
+	
 
 }
 
 void loop()
 {
-	if (digitalRead(menu_btn) == HIGH){
-		on_menu_mode();
-		press_menu_btn();
-		serial_banks_print();
+	menu_btn.tick();
+	save_btn.tick();
+	prev_btn.tick();
+	next_btn.tick();
+	a_btn.tick();
+	b_btn.tick();
+	c_btn.tick();
+	d_btn.tick();
 
-	} 
-	if (digitalRead(save_btn) == HIGH){
-		off_menu_mode();
-		serial_banks_print();
-	}
-	if (digitalRead(prev_btn) == HIGH){
-		press_prev_btn();
-		serial_banks_print();
-	}
-	if (digitalRead(next_btn) == HIGH){
-		press_next_btn();
-		serial_banks_print();
-	}
-	if (digitalRead(a_btn) == HIGH){
-		NO_preset = 0;
-		serial_banks_print();
-	}
-	if (digitalRead(b_btn) == HIGH){
-		NO_preset = 1;
-		serial_banks_print();
-	}
-	if (digitalRead(c_btn) == HIGH){
-		NO_preset = 2;
-		serial_banks_print();
-	}
-	if (digitalRead(d_btn) == HIGH){
-		NO_preset = 3;
-		serial_banks_print();
-	}
 }
-
-
